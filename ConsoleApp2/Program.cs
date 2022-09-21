28e0cfd979043ae0af2efad37ac0e62fe93cd58a
blich student <student-cyber@blich.co.il>
2022-09-21 13:59:49 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿class ConsoleApp2
+{
+    public static void Main()
+    {
+        double first_height, sec_height;
+        Console.WriteLine("Enter the two heights: ");
+        first_height = double.Parse(Console.ReadLine());
+        sec_height = double.Parse(Console.ReadLine());
+        if (first_height < sec_height)
+            Console.WriteLine("The ball is taking off!");
+        else
+            Console.WriteLine("The ball is retreating!");
+    }
+}
